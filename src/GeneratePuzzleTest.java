d0ba26ecaa3dfa85163b5773edf4499a2e9634bd
William <wdoughty@iastate.edu>
2019-05-13 12:27:38 -0500

    Added comments. Allowed for board generation until a solvable board is found

--- src/GeneratePuzzleTest.java
+++ src/GeneratePuzzleTest.java
@@ -2,9 +2,24 @@
 public class GeneratePuzzleTest {
 
     public static void main(String[] args){
-        SudokuBoard board = new SudokuBoard();
 
+
+        SudokuBoard board = new SudokuBoard();
+        String start ="";
+
+        while(!board.checkSolvedBoard()) {
+            board.clearSudokuBoard();
+            board.generateStartingValues(30);
+            board.printSudokuBoard();
+            start = board.makeString();
+            board.fillCell();
+        }
         board.printSudokuBoard();
+        String end = board.makeString();
+
+
+
+
 
 
 
--- src/SudokuCell.java
+++ src/SudokuCell.java
@@ -40,4 +40,6 @@ public class SudokuCell {
 		neighbors = cells;
 	}
 
+
+
 }
